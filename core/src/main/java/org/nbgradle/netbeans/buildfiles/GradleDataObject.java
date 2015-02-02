62704b21e3a930771a62610d3da6ab14a45431df
Radim Kubacki <radim@kubacki.cz>
2015-02-02 21:52:29 +0100

    Added MIME type / data object for Gradle files.

---
+++ core/src/main/java/org/nbgradle/netbeans/buildfiles/GradleDataObject.java
@@ -0,0 +1,89 @@
+package org.nbgradle.netbeans.buildfiles;
+
+import java.io.IOException;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.MIMEResolver;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiDataObject;
+import org.openide.loaders.MultiFileLoader;
+import org.openide.util.NbBundle.Messages;
+
+@Messages({
+    "LBL_Gradle_LOADER=Gradle Files"
+})
+@MIMEResolver.ExtensionRegistration(
+    displayName = "#LBL_Gradle_LOADER",
+    mimeType = "text/x-gradle+x-groovy",
+    extension = {"gradle"},
+    position = 2000)
+@DataObject.Registration(
+    mimeType = "text/x-gradle+x-groovy",
+    iconBase = "org/nbgradle/netbeans/project/gradle.png",
+    displayName = "#LBL_Gradle_LOADER",
+    position = 300)
+@ActionReferences({
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.OpenAction"),
+            position = 100,
+            separatorAfter = 200),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "Edit", id = "org.openide.actions.CutAction"),
+            position = 300),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"),
+            position = 400,
+            separatorAfter = 500),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"),
+            position = 600),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.RenameAction"),
+            position = 700,
+            separatorAfter = 800),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"),
+            position = 1100,
+            separatorAfter = 1200),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.ToolsAction"),
+            position = 1300),
+    @ActionReference(
+        path = "Loaders/text/x-gradle+x-groovy/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"),
+            position = 1400)
+})
+public class GradleDataObject extends MultiDataObject {
+
+    public GradleDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
+        super(pf, loader);
+        registerEditor("text/x-gradle+x-groovy", true);
+    }
+
+    @Override
+    protected int associateLookup() {
+        return 1;
+    }
+
+//    @MultiViewElement.Registration(
+//        displayName = "#LBL_Gradle_EDITOR",
+//    iconBase = "org/nbgradle/netbeans/project/gradle.png",
+//    mimeType = "text/x-gradle+x-groovy",
+//    persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED,
+//    preferredID = "Gradle",
+//    position = 1000)
+//    @Messages("LBL_Gradle_EDITOR=Gradle file")
+//    public static MultiViewEditorElement createEditor(Lookup lkp) {
+//        return new MultiViewEditorElement(lkp);
+//    }
+}
